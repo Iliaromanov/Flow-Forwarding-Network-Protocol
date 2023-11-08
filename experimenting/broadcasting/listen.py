ac6128e5b2b5e107e867714d05e57bcc0715a597
Iliaromanov <iliaromanov033@gmail.com>
2023-11-08 14:26:17 +0000

    experimenting with broadcasting to all nodes on network

---
+++ experimenting/broadcasting/listen.py
@@ -0,0 +1,11 @@
+import socket
+
+print("listener up")
+
+udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+udp_socket.bind(("0.0.0.0", 12345))
+
+data, addr = udp_socket.recvfrom(2048)
+
+print(f"Received data {data.decode()} from {addr}")
