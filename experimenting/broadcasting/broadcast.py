9f7e1e04de95b22da6bdee05071ba8f81604df85
Iliaromanov <iliaromanov033@gmail.com>
2023-11-11 11:44:08 +0000

    cleanup experiment style

--- experimenting/broadcasting/broadcast.py
+++ experimenting/broadcasting/broadcast.py
@@ -1,19 +1,25 @@
 import socket
 
 # Set up a UDP socket
-udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+udp_socket = socket.socket(
+    socket.AF_INET, socket.SOCK_DGRAM
+)
 
 # Enable broadcasting on the socket
-udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+udp_socket.setsockopt(
+    socket.SOL_SOCKET, socket.SO_BROADCAST, 1
+)
 
 # Define the broadcast address and port
 broadcast_address = "255.255.255.255"  # This is the broadcast address for most networks
-broadcast_port = 12345  # Choose a suitable port number
+broadcast_port = 12345  # Choose whatever port number
 
 # Your message to broadcast
 message = "Hello, this is a broadcast message!"
 
 # Send the broadcast message
-udp_socket.sendto(message.encode(), (broadcast_address, broadcast_port))
-print(dir(udp_socket))
+udp_socket.sendto(
+    message.encode(), 
+    (broadcast_address, broadcast_port)
+)
 print(f"Broadcasted message: '{message}'")
--- experimenting/broadcasting/listen.py
+++ experimenting/broadcasting/listen.py
@@ -2,7 +2,9 @@ import socket
 
 print("listener up")
 
-udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+udp_socket = socket.socket(
+    socket.AF_INET, socket.SOCK_DGRAM
+)
 
 udp_socket.bind(("0.0.0.0", 12345))
 
